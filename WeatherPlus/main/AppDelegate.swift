d4aa9c9e9629de82f59d2c6dfecd532cd8aa176e
ahmed sokar <ahmed.sokar@fawry.com>
2023-03-19 01:19:59 +0200

    Organizing app files.

---
+++ WeatherPlus/main/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  WeatherPlus
+//
+//  Created by admin on 19/03/2023.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    // MARK: - properties.
+    var window: UIWindow?
+    
+    // MARK: - methods.
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = UIStoryboard(name: "Main", bundle: nil).instantiateInitialViewController()
+        window?.makeKeyAndVisible()
+        return true
+    }
+}
+
